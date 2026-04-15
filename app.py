bd17830b611c67da3a53d9194e68c49768ab07e2
nicol4sr <69946776+nicol4sr@users.noreply.github.com>
2026-04-15 10:58:13 -0400

    Change page title from 'VAR Chatbot' to 'Mundial Chatbot'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 # Configuración de página con temática futbolera
-st.set_page_config(page_title="VAR Chatbot", page_icon="⚽")
+st.set_page_config(page_title="Mundial Chatbot", page_icon="⚽")
 
 st.markdown("""
     <style>
